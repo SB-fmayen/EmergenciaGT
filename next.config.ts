062b72584b38369d7fecef8fb7d71588158477f4
Alfredo Fajardo <fajardomayen55@gmail.com>
2025-08-28 05:25:34 +0000

    Mira sale este error

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-});
-
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -33,6 +25,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
-
-    
+module.exports = nextConfig;
