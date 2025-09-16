5fe032441f41c833a1ad129c7eaa96f5f68ca7a7
Alfredo Fajardo <fajardomayen55@gmail.com>
2025-09-16 00:58:25 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from '@/app/(mobile)/layout';
 
 export const metadata: Metadata = {
   title: 'EmergenciaGT',
@@ -23,7 +25,9 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/cuerpo-bomberos-logo.png" />
       </head>
       <body className="font-body antialiased">
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
         <Toaster />
       </body>
     </html>
